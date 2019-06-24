384c1f90c4ea7d982c7d03203e476929f2c8dff5
AllChit <aungkophonemyint@gmail.com>
2019-06-24 09:48:23 +0600

    new hospital detail

--- app/src/main/java/com/example/clinic_project/Activity/HospitalDetailActivity.java
+++ app/src/main/java/com/example/clinic_project/Activity/HospitalDetailActivity.java
@@ -22,8 +22,8 @@ public class HospitalDetailActivity extends AppCompatActivity {
 
     private RetrofitService service;
     private String token;
-    private ImageView imageView;
-    private TextView txtname,txtlocation,txtphoneno,txttown;
+    private ImageView imageView,imgSpecial;
+    private TextView txtname,txtlocation,txtphoneno,txttown,tvabout,textabout;
     private int buildingId = -1;
     private int typeId = 2;
 
@@ -37,9 +37,12 @@ public class HospitalDetailActivity extends AppCompatActivity {
 
     private void initActivity() {
 
-        imageView = findViewById(R.id.profile1);
+        imageView = findViewById(R.id.imageView);
+        imgSpecial = findViewById(R.id.imgSpecial);
         txtname = findViewById(R.id.tvName);
         txtlocation = findViewById(R.id.tvLocation);
+        textabout = findViewById(R.id.textabout);
+        tvabout= findViewById(R.id.tvabout);
         txtphoneno = findViewById(R.id.tvphoneNo);
         txttown = findViewById(R.id.town);
         token = Token.MyToken.getToken();
@@ -76,8 +79,11 @@ public class HospitalDetailActivity extends AppCompatActivity {
                         txtname.setText(response.body().buildingDetails.get(0).name);
                         txtlocation.setText(response.body().buildingDetails.get(0).townName);
 
-                        txtphoneno.setText(response.body().buildingDetails.get(0).phoneNumber.get(0));
-                        txttown.setText(response.body().buildingDetails.get(0).address);
+//                        txtphoneno.setText(response.body().buildingDetails.get(0).phoneNumber.get(0));
+//                        txttown.setText(response.body().buildingDetails.get(0).address);
+
+                        String About =response.body().buildingDetails.get(0).address;
+                        textabout.setText(response.body().buildingDetails.get(0).name);
                     }
                 }
             }
